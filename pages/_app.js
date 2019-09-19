7d5a8caf550bd5997009cecba83ede48358dec5f
elad ben aderet <eladonline@walla.com>
2019-09-19 21:37:41 +0300

    fix form and _app

--- pages/_app.js
+++ pages/_app.js
@@ -5,16 +5,6 @@ import "style/core.scss";
 import "style/theme.less";
 
 export default class MyApp extends App {
-  static async getInitialProps({ Component, router, ctx }) {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
   render() {
     const { Component, pageProps } = this.props;
     return (
