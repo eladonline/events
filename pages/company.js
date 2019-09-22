702553eb35012486ee17a465a1aa394581e6ac8e
eladonline <elad.ben.aderet@herolo.co.il>
2019-09-22 15:31:32 +0300

    remove company

--- pages/company.js
+++
@@ -1,3 +0,0 @@
-import Company from 'src/screens/Company.js';
-
-export default () => <Company />;
--- src/screens/Company.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import EventPage from 'src/components/eventPage/EventPage.js';
-import data from 'src/data/products/company/data.json'
-
-const Company = () => {
-  return <EventPage {...data}/>;
-};
-
-export default Company;
