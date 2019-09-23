68700fc588f35e6216550247b212930aad204f24
elad ben aderet <eladonline@walla.com>
2019-09-23 22:41:57 +0300

    change analityc

--- pages/vr-accessories.js
+++ pages/vr-accessories.js
@@ -5,7 +5,7 @@ import ReactGA from "react-ga";
 
 export default () => {
   useEffect(() => {
-    ReactGA.initialize("UA-148411020-1");
+    ReactGA.initialize("UA-148541360-1");
     ReactGA.pageview(window.location.pathname + window.location.search);
   }, []);
   return <VrAccessories />;
