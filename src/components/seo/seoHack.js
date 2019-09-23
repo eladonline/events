426be41bb00bc472141e9f6eb3eff7d745770293
elad ben aderet <eladonline@walla.com>
2019-09-23 22:33:18 +0300

    hack seo

--- src/components/seo/seoHack.js
+++ src/components/seo/seoHack.js
@@ -1,20 +1,65 @@
 const SeoHack = () => {
-  return <div style={style}>
-     <h1>משקפי מציאות מדומה, משקפי מציאות מדומה תל אביב, משקפי מציאות מדומה בזול, משפקי vr, משקפי vr מחיר</h1>
-     <h2>משקפי מציאות מדומה במחיר זול אוקלוס ריפט אס htc vive מחיר vavle index מחיר חנות למציאות מדומה</h2>
-     <p> </p>
-    <a href="https://www.oculus.com/medium/">וירטואל ריאליטי</a>
-    <a href="https://medium.com/">חנות מציאות מדומה</a>
-    <a href="https://medium.com/wonda-vr">משקפי מציאות מדומה</a>
-    <a href="https://www.pcmag.com/compare/368050/oculus-quest-vs-oculus-rift-s-which-vr-headset-should-you">אוקולוס ריפט אס</a>
-    <a href="https://www.windowscentral.com/oculus-rift-s-vs-htc-vive">אוקולוס</a>
-    <a href="https://www.techradar.com/news/wearables/htc-vive-vs-oculus-rift-1301375">htc vive חנות</a>
-    <a href="https://www.techradar.com/news/wearables/htc-vive-vs-oculus-rift-1301343">משקפי vr</a>
-    <a href="https://www.newegg.com/vr/guides/oculus-rift-vs-htc-vive.html">חנות מציאות מדומה חולון</a>
-  </div>;
+  return (
+    <div className="seo-hack">
+      <h1>
+        משקפי מציאות מדומה, משקפי מציאות מדומה תל אביב, משקפי מציאות מדומה בזול,
+        משקפי vr, משקפי vr מחיר
+      </h1>
+      <h2>
+        משקפי מציאות מדומה במחיר זול אוקלוס ריפט אס htc vive מחיר vavle index
+        מחיר חנות למציאות מדומה
+      </h2>
+      <p>
+        מהם משקפי של מציאות מדומה? מהם משקפי וגם מציאות מדומה? להבין את התחום
+        ויש ואת האפשרויות הרבותאם שהוא מעמיד לרשותנו
+      </p>
+      <p>
+        מה זה מציאות מדומה ? המושג 'מציאות מדומה' נזרק די שכן הרבה בתקופה היא
+        מושג המתייחס למכשיר המסוגל לייצר עולם ועוד ממוחשב שלם הסובב אותך. זאת
+        בניגוד למציאות רבודה, כאן מדובר בהוספת שכבות על העולם הפיסי שלך באיכות
+        של המסכים הקטנים (חזרה למסכי LCD), יש גם את נושא המציאות מדומה
+        לסמארטפונים
+      </p>
+      <p>
+        מוצרי מציאות מדומה, בדומה לאוקלוס ריפט, שלום מתחברים למחשב או לקונסולת
+        משחקים ומשפרים בצורה משמעותית את חווית המשחק שלכם. גם לגם ניתן למצוא או
+        ש... אפילו משקפי מציאות מדומה שהם בעצמם קונסולה שלמה שכוללת כבר מסך ומ
+        פשוט מאוד חשב בתוך המשקפיים.
+      </p>
+      <p>
+        משקפי מציאות מדומה טובות, עושות שימוש utz בשילוב עדשות בזוית שונה לכל
+        עין
+      </p>
+      <p>למה לבחור במשקפי מציאות מדומה ?</p>
+      <p>
+        הגדרת השימוש עבור המשקפיים שתרצו תוכל לצמצם משמעותית את המבחר וטווח
+        המחירים.
+      </p>
+      <p>
+        לשחקנים מתחילים ומתקדמים מומלץ לרכוש בכל מקרה בנוסף לערכת המשקפי מציאות
+        מדומה גם ג'ויסטיק תואם או לפחות לוודא שמשקפיים יהיו מסוגלות להתחבר
+        לג'ויסטיק בעתיד..
+      </p>
+      <a href="https://www.oculus.com/medium/">וירטואל ריאליטי</a>
+      <a href="https://medium.com/">חנות מציאות מדומה</a>
+      <a href="https://medium.com/wonda-vr">משקפי מציאות מדומה</a>
+      <a href="https://www.pcmag.com/compare/368050/oculus-quest-vs-oculus-rift-s-which-vr-headset-should-you">
+        אוקולוס ריפט אס
+      </a>
+      <a href="https://www.windowscentral.com/oculus-rift-s-vs-htc-vive">
+        אוקולוס
+      </a>
+      <a href="https://www.techradar.com/news/wearables/htc-vive-vs-oculus-rift-1301375">
+        htc vive חנות
+      </a>
+      <a href="https://www.techradar.com/news/wearables/htc-vive-vs-oculus-rift-1301343">
+        משקפי vr
+      </a>
+      <a href="https://www.newegg.com/vr/guides/oculus-rift-vs-htc-vive.html">
+        חנות מציאות מדומה חולון
+      </a>
+    </div>
+  );
 };
 
-const style = { position: "absolute", Zindex: "-1", fontSize:'22px' };
-
-
-export default SeoHack
+export default SeoHack;
