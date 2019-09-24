15588f9667f197e221b6bffd70cff11b2397a100
elad ben aderet <eladonline@walla.com>
2019-09-24 21:26:34 +0300

    fix supply

--- src/components/seo/seoHack.js
+++ src/components/seo/seoHack.js
@@ -46,26 +46,59 @@ const SeoHack = () => {
         מדומה גם ג'ויסטיק תואם או לפחות לוודא שמשקפיים יהיו מסוגלות להתחבר
         מציאות וירטואלית תל אביב לג'ויסטיק בעתיד.. vr-era
       </p>
-      <a title="מידע על אוקלוס במדיום" href="https://www.oculus.com/medium/">מידע על אוקלוס במדיום</a>
-      <a title='אילו משקפי המציאות מדומה הכי כדאי לקנות' href="https://www.techradar.com/news/the-best-vr-headset">משקפי המציאות מדומה הכי כדאי לקנות</a>
-      <a title='wonda vr' href="https://medium.com/wonda-vr">wonda vr</a>
-      <a title="אוקולוס קווסט או אוקולוס ריפד אס" href="https://www.pcmag.com/compare/368050/oculus-quest-vs-oculus-rift-s-which-vr-headset-should-you">
-      אוקולוס קווסט או אוקולוס ריפד אס
+      <a title="מידע על אוקלוס במדיום" href="https://www.oculus.com/medium/">
+        מידע על אוקלוס במדיום
       </a>
-      <a title="אוקולוס ריפט או htc vive" href="https://www.windowscentral.com/oculus-rift-s-vs-htc-vive">
-      אוקולוס ריפט אס  או htc vive
+      <a
+        title="אילו משקפי המציאות מדומה הכי כדאי לקנות"
+        href="https://www.techradar.com/news/the-best-vr-headset"
+      >
+        משקפי המציאות מדומה הכי כדאי לקנות
       </a>
-      <a title="טקרדאר" href="https://www.techradar.com/news/wearables/htc-vive-vs-oculus-rift-1301375">
-      אוקולוס ריפט או htc vive
+      <a title="wonda vr" href="https://medium.com/wonda-vr">
+        wonda vr
       </a>
-   
-      <a title="pimax or valve index" href="https://www.reddit.com/r/virtualreality/comments/bprrt1/pimax_5k_vs_valve_index/">
+      <a
+        title="אוקולוס קווסט או אוקולוס ריפד אס"
+        href="https://www.pcmag.com/compare/368050/oculus-quest-vs-oculus-rift-s-which-vr-headset-should-you"
+      >
+        אוקולוס קווסט או אוקולוס ריפד אס
+      </a>
+      <a
+        title="אוקולוס ריפט או htc vive"
+        href="https://www.windowscentral.com/oculus-rift-s-vs-htc-vive"
+      >
+        אוקולוס ריפט אס או htc vive
+      </a>
+      <a
+        title="טקרדאר"
+        href="https://www.techradar.com/news/wearables/htc-vive-vs-oculus-rift-1301375"
+      >
+        אוקולוס ריפט או htc vive
+      </a>
+
+      <a
+        title="pimax or valve index"
+        href="https://www.reddit.com/r/virtualreality/comments/bprrt1/pimax_5k_vs_valve_index/"
+      >
         פימקס או וואלב אינדקס
       </a>
-      <a title="virtual reality blog" href="https://medium.com/@eladbenaderet/virtual-reality-era-97d29d151c3?sk=b56796c596dd372c5480c5bd63a5a881">
+      <a
+        title="virtual reality blog"
+        href="https://medium.com/@eladbenaderet/virtual-reality-era-97d29d151c3?sk=b56796c596dd372c5480c5bd63a5a881"
+      >
         בלוג שלנו
       </a>
-      <a title="fxp תראו מה רשמו לגבי המציאות המדומה" href="https://www.fxp.co.il/showthread.php?t=19888235&p=199360798#post199360798">
+      <a
+        title="fxp תראו מה רשמו לגבי המציאות המדומה"
+        href="https://www.fxp.co.il/showthread.php?t=19888235&p=199360798#post199360798"
+      >
+        בלוג שלנו
+      </a>
+      <a
+        title="סרטון שלי משחק ביוטיוב"
+        href="https://www.youtube.com/watch?v=feCUDZNZFPY&feature=youtu.be"
+      >
         בלוג שלנו
       </a>
     </div>
